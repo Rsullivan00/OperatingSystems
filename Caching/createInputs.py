1127d1aa79882d808a71463870277253bbc59b3a
Rick Sullivan <rick@ricksullivan.net>
2014-11-18 13:58:07 -0800

    Fixing range of python scripts.

--- Caching/createInputs.py
+++ Caching/createInputs.py
@@ -9,19 +9,20 @@ ID_RANGE = 50
 def writeInputFiles(numInputLines=NUM_INPUT_LINES, idRange=ID_RANGE):
     f = open("inputs/uniform.txt", 'w')
     for i in range(0, numInputLines):
-        r = random.randint(0, idRange)
+        r = random.randint(0, idRange - 1)
         f.write(str(r) + '\n')
 
     f = open("inputs/triangular.txt", 'w')
     for i in range(0, numInputLines):
-        r = int(random.triangular(0, idRange))
+        r = int(random.triangular(0, idRange - 1))
         f.write(str(r) + '\n')
 
     f = open("inputs/onePage.txt", 'w')
     for i in range(0, numInputLines):
-        r = random.randint(0, idRange)
+        r = random.randint(0, idRange - 1)
         # 25% of the time, request the same page
-        if r < idRange/4:
+        r2 = random.randint(0, 4)
+        if r2 == 0:
             r = 0
         f.write(str(r) + '\n')
 
